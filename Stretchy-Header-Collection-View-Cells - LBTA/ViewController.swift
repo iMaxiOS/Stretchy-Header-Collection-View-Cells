28ef5c3b252d88c24b17baa9627ed5650d5b3244
iMakca <ukr-maksim1@yandex.ru>
2019-04-15 19:48:39 +0300

    add project

---
+++ Stretchy-Header-Collection-View-Cells - LBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stretchy-Header-Collection-View-Cells - LBTA
+//
+//  Created by Maxim Granchenko on 4/15/19.
+//  Copyright © 2019 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
